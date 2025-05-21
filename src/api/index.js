db7da1b5a5844ce64614e45c06d64f9c99081741
WowDespacito <wangzhi_wow@163.com>
2025-05-21 22:29:06 +0800

    最终上线版本

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,11 @@
 // 该文件用于存放所有的 API 接口
 // 获取blogList
+
 export const getBlogList = async (params = {}) =>{
   try{
     const quaryString = new URLSearchParams(params).toString();
     const res = await fetch(
-      `http://localhost:3000/api/getList?${quaryString}`,{
+      `https://blog.wowdespacito.pro/api/getList?${quaryString}`,{
         method: 'GET'
       }
     );
@@ -19,7 +20,7 @@ export const getBlogList = async (params = {}) =>{
 export const getBlogDetail = async (id) =>{
   try{
     const res = await fetch(
-      `http://localhost:3000/api/getDetail?id=${id}`,{
+      `https://blog.wowdespacito.pro/api/getDetail?id=${id}`,{
         method: 'GET'
       }
     );
